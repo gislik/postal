23397836677803f44fcfe016975bf579525b8169
Gísli Kristjánsson <gislik@hamstur.is>
2013-06-19 10:21:12 +0000

    Updated location for pom.properties to reflect the forked version

--- src/postal/support.clj
+++ src/postal/support.clj
@@ -73,7 +73,7 @@
        (format "<%s.%s@%s>" onlychars epoch host))))
 
 (defn pom-version []
-  (let [pom "META-INF/maven/com.draines/postal/pom.properties"
+  (let [pom "META-INF/maven/gislik/postal/pom.properties"
         props (doto (Properties.)
                 (.load (-> pom io/resource io/input-stream)))]
     (.getProperty props "version")))
